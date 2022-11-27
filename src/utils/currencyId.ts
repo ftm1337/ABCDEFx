49a784069f8dc08ca78f7af57da6e0b0951c7fc4
ftm1337 <ftm1337@protonmail.com>
2022-11-27 07:08:31 -0500

    Generalize..

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -5,6 +5,3 @@ export function currencyId(currency: Currency): string {
   if (currency instanceof Token) return currency.address
   throw new Error('invalid currency')
 }
-
-
-currencyIdFromChainId
