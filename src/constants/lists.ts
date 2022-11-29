e90888322efedf54198c810fa19cfbb5ec6ef909
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2022-11-29 06:39:52 -0500

    https://ftm.guru/tokenlists/abcdefx.json

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://ftm.guru/tokenlists/abcdefx.json'
+export const DEFAULT_TOKEN_LIST_URL = ''
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   //'https://raw.githubusercontent.com/mtvguru/vax-list/main/docs/vax-list.json',
