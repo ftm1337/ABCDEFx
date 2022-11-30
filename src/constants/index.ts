dcc839fa2bb295c70e2268b689142ec2a5ca3363
ftm1337 <ftm1337@protonmail.com>
2022-11-30 05:39:48 -0500

    sdk-0.0.5

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -151,7 +151,7 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
 export const NetworkContextName = 'NETWORK'
 
 // default allowed slippage, in bips
-export const INITIAL_ALLOWED_SLIPPAGE = 200
+export const INITIAL_ALLOWED_SLIPPAGE = 111111111///200;///TESTING! Change in prod!!!
 // 20 minutes, denominated in seconds
 export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20
 
