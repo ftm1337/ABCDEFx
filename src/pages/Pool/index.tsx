c0453822d0b237978771e7006fe545a650f4cf1e
ftm1337 <ftm1337@protonmail.com>
2022-12-01 22:30:46 -0500

    COIN.logo

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -115,7 +115,7 @@ export default function Pool() {
 
       <div style={{ display: 'flex', alignItems: 'center', marginTop: '1.5rem' }}>
         <ButtonSecondary as={Link} style={{ width: 'initial' }} to='/pool'>
-          Trade Fee generates Protocol-Owned Liquidity.<br>Stake your LP tokens to earn further yield!
+          Trade Fee generates Protocol-Owned Liquidity.<br />Stake your LP tokens to earn further yield!
         </ButtonSecondary>
       </div>
     </>
