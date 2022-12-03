24b09f85b3ddebb5b4e9f79cbcc13b682a7e2e86
ftm1337 <ftm1337@protonmail.com>
2022-12-03 07:30:46 -0500

    Release 0.1.0

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -2,8 +2,8 @@ import { Currency, ETHER, Token } from '@ftm1337/abcdefx-sdk'
 import { COIN_symbol } from '../utils/coinMeta'
 import { useActiveWeb3React } from '../hooks'
 
+const { chainId } = useActiveWeb3React()
 export function currencyId(currency: Currency): string {
-  const { chainId } = useActiveWeb3React()
   if (currency === ETHER) return COIN_symbol(chainId)
   if (currency instanceof Token) return currency.address
   throw new Error('invalid currency')
