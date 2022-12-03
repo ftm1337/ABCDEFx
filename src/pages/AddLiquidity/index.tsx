228b328d2b4a3ce8371935fe9c45a36585dcb9d3
ftm1337 <ftm1337@protonmail.com>
2022-12-03 11:25:17 -0500

    Use Wrapped at Minimal Position Cards

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -1,6 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import { TransactionResponse } from '@ethersproject/providers'
-import { Currency, /*currencyEquals,*/ ETHER, TokenAmount, WETH } from '@ftm1337/abcdefx-sdk'
+import { Currency, /*currencyEquals,*/ ETHER, TokenAmount/*, WETH*/ } from '@ftm1337/abcdefx-sdk'
 import React, { useCallback, useContext, useState } from 'react'
 import { Plus } from 'react-feather'
 import ReactGA from 'react-ga'
