79874378a061cc2740e9d21cf370a7f74bcff7ff
ftm1337 <ftm1337@protonmail.com>
2022-12-03 07:58:02 -0500

    Release 0.1.0

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -104,6 +104,7 @@ export function useToken(tokenAddress?: string): Token | undefined | null {
 }
 
 export function useCurrency(currencyId: string | undefined): Currency | null | undefined {
+  const { chainId } = useActiveWeb3React()
   const isETH = currencyId?.toUpperCase() === COIN_symbol(chainId)
   const token = useToken(isETH ? undefined : currencyId)
   return isETH ? ETHER : token
